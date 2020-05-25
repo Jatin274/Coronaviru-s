aaf5a617bf2bdc31878c7418d4165b7d8383d189
Jatin274 <65862731+Jatin274@users.noreply.github.com>
2020-05-25 12:23:28 +0530

    Update settings.py

--- Coronavirus/settings.py
+++ Coronavirus/settings.py
@@ -131,7 +131,9 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
-https://docs.djangoproject.com/en/2.1/howto/static-files/
+#https://docs.djangoproject.com/en/2.1/howto/static-files/
+heroku config:set DISABLE_COLLECTSTATIC=1
+
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
