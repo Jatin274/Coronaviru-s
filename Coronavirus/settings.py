3eaf3a2afdeddc150afc2bdb9150c06b63f13e5a
Jatin274 <65862731+Jatin274@users.noreply.github.com>
2020-05-25 11:11:54 +0530

    Update settings.py

--- Coronavirus/settings.py
+++ Coronavirus/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/mediaFile/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "Upload")
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
