74fc911b8ee3b38c9502538b890385e2d30b0ae2
Jatin274 <65862731+Jatin274@users.noreply.github.com>
2020-05-25 11:13:53 +0530

    Update settings.py

--- Coronavirus/settings.py
+++ Coronavirus/settings.py
@@ -128,3 +128,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "Upload")
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
